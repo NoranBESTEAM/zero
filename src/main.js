8b3dc9b889e41a9419770c06b4991dc423d5e82f
NoranBESTEAM <nmohammed@be-steam.com>
2023-11-05 11:06:59 +0200

    Install Project & Packages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,35 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+// import Bootstrap
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+//import Normalize
+import "normalize.css";
+// import flag icon
+import FlagIcon from "vue-flag-icon";
+// import Wow Library
+import 'animate.css';
+//import the fontawesome core
+import { library } from "@fortawesome/fontawesome-svg-core";
+//import font awesome icon component
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+//import specific icons
+//Regular Icons
+import { far } from "@fortawesome/free-regular-svg-icons";
+//Solid Icons
+import { fas } from "@fortawesome/free-solid-svg-icons";
+//Brands Icons
+import { fab } from "@fortawesome/free-brands-svg-icons";
+//add icons to the library
+library.add(fas, far, fab);
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(store);
+//Add font awesome icon component
+app.component("font-awesome-icon", FontAwesomeIcon);
+// Add Flag Icon
+app.use(FlagIcon);
+app.use(router);
+app.mount("#app");
