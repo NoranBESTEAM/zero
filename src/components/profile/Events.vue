c7c31cbee68d17778f58076c60826705f8dee7ac
NoranBESTEAM <nmohammed@be-steam.com>
2023-11-29 13:37:19 +0200

    Events

---
+++ src/components/profile/Events.vue
@@ -0,0 +1,47 @@
+<template>
+    <section id="events">
+
+        <div class="container">
+
+            <ProfileNavbar />
+            <router-view />
+
+            <h5 class="mt-4">All events</h5>
+
+            <p class="mt-3">No events</p>
+
+        </div>
+
+    </section>
+</template>
+
+<script>
+import ProfileNavbar from '@/components/profile/ProfileNavbar.vue';
+export default {
+    name: 'Events',
+    components: {
+        ProfileNavbar,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+#events {
+
+    margin-top: 64px;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background: $my-gradient;
+    min-height: 100vh;
+
+    h5 {
+        font-size: 18px;
+        color: $white;
+    }
+
+    p {
+        font-size: 16px;
+        color: $label-color;
+    }
+}
+</style>
--- src/views/profile/ProfileEventsView.vue
+++ src/views/profile/ProfileEventsView.vue
@@ -1,13 +1,24 @@
 <template>
-
+    <!-- import navbar in main.js file -->
+    <Navbar />
+    <router-view />
+    <!-- Start Events -->
+    <Events />
+    <!-- End Events -->
+    <!-- Start Footer -->
+    <!-- import Footer in main.js file -->
+    <Footer />
+    <!-- End Footer -->
 </template>
 
 <script>
+import Events from '@/components/profile/Events.vue';
 export default {
     name: 'ProfileEventsView',
+    components: {
+        Events
+    }
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
